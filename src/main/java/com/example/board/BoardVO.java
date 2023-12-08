6dc0445073c264c189bd0569afc3ade8651b9a85
immanuelk1m <kse0119@naver.com>
2023-12-08 14:53:17 +0900

    first commit

---
+++ src/main/java/com/example/board/BoardVO.java
@@ -0,0 +1,132 @@
+package com.example.board;
+
+import java.util.Date;
+
+public class BoardVO
+{
+        private String prfpdfrom;
+        private String prfpdto;
+        private String fcltynm;
+        private String prfcast;
+        private String prfruntime;
+        private String prfage;
+        private String pcseguidance;
+        private String poster;  // Same here
+        private String prfnm;
+
+        private String agerange;
+        private int doimage;
+        private String location;
+
+    public String getAgerange() {
+        return agerange;
+    }
+
+    public void setAgerange(String agerange) {
+        this.agerange = agerange;
+    }
+
+    public int getDoimage() {
+        return doimage;
+    }
+
+    public void setDoimage(int doimage) {
+        this.doimage = doimage;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPrfpdfrom() {
+        return prfpdfrom;
+    }
+
+    public void setPrfpdfrom(String prfpdfrom) {
+        this.prfpdfrom = prfpdfrom;
+    }
+
+    public String getPrfpdto() {
+        return prfpdto;
+    }
+
+    public void setPrfpdto(String prfpdto) {
+        this.prfpdto = prfpdto;
+    }
+
+    public String getFcltynm() {
+        return fcltynm;
+    }
+
+    public void setFcltynm(String fcltynm) {
+        this.fcltynm = fcltynm;
+    }
+
+    public String getPrfcast() {
+        return prfcast;
+    }
+
+    public void setPrfcast(String prfcast) {
+        this.prfcast = prfcast;
+    }
+
+
+    public String getPrfruntime() {
+        return prfruntime;
+    }
+
+    public void setPrfruntime(String prfruntime) {
+        this.prfruntime = prfruntime;
+    }
+
+    public String getPrfage() {
+        return prfage;
+    }
+
+    public void setPrfage(String prfage) {
+        this.prfage = prfage;
+    }
+
+    public String getPcseguidance() {
+        return pcseguidance;
+    }
+
+    public void setPcseguidance(String pcseguidance) {
+        this.pcseguidance = pcseguidance;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+
+    public String getPrfnm() {
+        return prfnm;
+    }
+
+    public void setPrfnm(String prfnm) {
+        this.prfnm = prfnm;
+    }
+
+    public int getBid() {
+        return bid;
+    }
+
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    private int bid;
+
+
+}
+
+
